3e53639d9606ef7383fe669f683a77e36e596385
Sanket Botre <sanketbotre24@gmail.com>
2025-10-01 23:23:34 +0530

    pool creation

--- config/db-client.js
+++ config/db-client.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2/promise";
 import { env } from "./env.js";
 
-export const db = await mysql.createConnection({
+export const db = mysql.createPool({
         host: env.DATABASE_HOST,
         user: env.DATABASE_USER,
         password: env.DATABASE_PASSWORD,
         database: env.DATABASE_NAME,
         port: env.DATABASE_PORT,
         ssl: { rejectUnauthorized: true },
+        waitForConnections: true,
+        connectionLimit: 10,      
+        queueLimit: 0
 })
